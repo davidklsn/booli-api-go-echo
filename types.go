7263a07c31a853042adf20222481cf83715c6282
David Karlsson <sjappskalle@gmail.com>
2023-07-12 09:21:52 +0200

    Update table

--- types.go
+++ types.go
@@ -7,13 +7,15 @@ import (
 
 type UserData struct {
 	gorm.Model
-	UserID      string `gorm:"unique"`
-	ResidenceID string
-	Meta        datatypes.JSON
+	UserID       string `gorm:"unique"`
+	Residences   datatypes.JSON
+	ActivityData datatypes.JSON
+	CustomInfo   datatypes.JSON
 }
 
 type Request struct {
-	ID          string                 `json:"id"`
-	ResidenceID string                 `json:"residenceId"`
-	Meta        map[string]interface{} `json:"meta"`
+	ID        string                 `json:"id"`
+	Residence map[string]interface{} `json:"residence"`
+	Activity  map[string]interface{} `json:"activity"`
+	Info      map[string]interface{} `json:"info"`
 }
