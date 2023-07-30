6292091d58595089ce9d0046e1c1d6b82d4f8313
David Karlsson <sjappskalle@gmail.com>
2023-07-30 16:37:38 +0200

    Displayer users in table

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -3,9 +3,17 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/davidklsn/booli-api-go/handlers"
+	"github.com/davidklsn/booli-api-go/types"
 	"github.com/labstack/echo/v4"
 )
 
 func Index(c echo.Context) error {
-	return c.Render(http.StatusOK, "hello", "World")
+	var users []types.UserData
+	users, _ = handlers.GetUsers()
+	data := map[string]interface{}{
+		"Users": users,
+	}
+
+	return c.Render(http.StatusOK, "start", data)
 }
